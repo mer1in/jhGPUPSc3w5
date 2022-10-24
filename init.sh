0cc99bc4f33336fae98c6605e61875505259808f
Roman <roman.gorelik@gmail.com>
2022-10-24 17:19:57 +0300

    WIP /Users/romangorelik/study/coursera/cuda-at-scale-for-the-enterprise/week5-fin-proj/init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,13 @@
 echo "Fetch submodules"
 git submodule update --init --recursive
 
+echo "Build ffmpeg"
+( cd ffmpeg && ./configure --disable-x86asm --prefix=`pwd` && make && make install && make examples )
+
+echo "Run decoding test"
 (
-    cd ffmpeg && ./configure --disable-x86asm --prefix=`pwd` && make && make install && make examples
+    mkdir -p ~/project/out
+    ./ffmpeg/bin/ffmpeg -i ./video/P1033651.mp4 mpeg1.mpeg
+    ./ffmpeg/doc/examples/decode_video ./mpeg1.mpeg ~/project/out/frame
+    ls -la ~/project/out/
 )
