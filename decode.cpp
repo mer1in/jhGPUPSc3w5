4362cd339d188c855046c2fe4ae9cb5f55f905e1
Roman <roman.gorelik@gmail.com>
2022-10-28 13:15:49 +0300

    WIP decode.cpp

--- decode.cpp
+++ decode.cpp
@@ -104,26 +104,6 @@ static int output_video_frame(AVFrame *frame)
     return 0;
 }
 
-static int output_audio_frame(AVFrame *frame)
-{
-    size_t unpadded_linesize = frame->nb_samples * av_get_bytes_per_sample(frame->format);
-    printf("audio_frame n:%d nb_samples:%d pts:%s\n",
-           audio_frame_count++, frame->nb_samples,
-           av_ts2timestr(frame->pts, &audio_dec_ctx->time_base));
-
-    /* Write the raw audio data samples of the first plane. This works
-     * fine for packed formats (e.g. AV_SAMPLE_FMT_S16). However,
-     * most audio decoders output planar audio, which uses a separate
-     * plane of audio samples for each channel (e.g. AV_SAMPLE_FMT_S16P).
-     * In other words, this code will write only the first audio channel
-     * in these cases.
-     * You should use libswresample or libavfilter to convert the frame
-     * to packed data. */
-    fwrite(frame->extended_data[0], 1, unpadded_linesize, audio_dst_file);
-
-    return 0;
-}
-
 static int decode_packet(AVCodecContext *dec, const AVPacket *pkt)
 {
     int ret = 0;
@@ -154,8 +134,6 @@ static int decode_packet(AVCodecContext *dec, const AVPacket *pkt)
             ret = output_video_frame(frame);
             //ppm_save(frame->data[0], frame->linesize[0], frame->width, frame->height, frame->data);
         }
-        else
-            ret = output_audio_frame(frame);
 
         av_frame_unref(frame);
         if (ret < 0)
