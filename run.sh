0f08a35070c07a30fe2562cea1632d173ab4feab
Roman <roman.gorelik@gmail.com>
2022-10-28 10:11:51 +0300

    WIP run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,5 @@ cd ffmpeg
 gcc decode.cpp -Llib -lavcodec -lavdevice -lavfilter -lavformat -lavutil -lpostproc -lswresample \
     -lswscale -lpthread -lz -llzma -lm \
     && echo "decode compiled" || echo "Failed to compile decode"
-./a.out
+mkdir out
+./a.out ../video/P1033651.mp4 out/out
