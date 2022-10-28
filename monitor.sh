f7832c6f03c1d8c6884101869d10085d40502524
Roman <roman.gorelik@gmail.com>
2022-10-28 12:19:44 +0300

    WIP monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -5,7 +5,9 @@ WHAT='./run.sh'
 while [[ true ]]
 do
     git pull -r >/dev/null
-    chsum2=`find . -type f ! -name "\.*\.sw*" -exec md5sum {} \;`
+    chsum2=`find . -type d \( -path ./ffmpeg/out -o -path ./ffmpeg/my \) -prune -o \
+        -type f ! -name "\.*\.sw*" -exec md5sum {} \;`
+#    chsum2=`find . -type f ! -name "\.*\.sw*" -exec md5sum {} \;`
     if [[ $chsum1 != $chsum2 ]]
     then
         chsum1=$chsum2
