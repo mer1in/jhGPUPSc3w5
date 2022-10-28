3f7da1d93cba73870ff30b2f1bdf9bc0dbea6682
Roman <roman.gorelik@gmail.com>
2022-10-28 13:10:07 +0300

    WIP run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cp decode.c ffmpeg/
+cp decode.cpp ffmpeg/
 cd ffmpeg
 mkdir out
 mkdir my
-#gcc decode.cpp -Llib -lavcodec -lavdevice -lavfilter -lavformat -lavutil -lpostproc -lswresample \
-#    -lswscale -lpthread -lz -llzma -lm -fpermissive -D__STDC_CONSTANT_MACROS \
-#    && echo "decode compiled" && ./a.out ../video/P1033651.mp4 out/out || echo "Failed to compile decode"
+gcc decode.cpp -Llib -lavcodec -lavdevice -lavfilter -lavformat -lavutil -lpostproc -lswresample \
+    -lswscale -lpthread -lz -llzma -lm -fpermissive -D__STDC_CONSTANT_MACROS \
+    && echo "decode compiled" && ./a.out ../video/P1033651.mp4 out/out || echo "Failed to compile decode"
 
-cc decode.c -Llib -lavcodec -lavdevice -lavfilter -lavformat -lavutil -lpostproc -lswresample \
-    -lswscale -lpthread -lz -llzma -lm -o my/decode \
-    && echo "decode compiled" && ./my/decode ../video/P1033651.mp4 out/out || echo "Failed to compile decode"
+#cc decode.c -Llib -lavcodec -lavdevice -lavfilter -lavformat -lavutil -lpostproc -lswresample \
+#    -lswscale -lpthread -lz -llzma -lm -o my/decode \
+#    && echo "decode compiled" && ./my/decode ../video/P1033651.mp4 out/out || echo "Failed to compile decode"
 
