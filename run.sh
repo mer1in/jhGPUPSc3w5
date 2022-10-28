29c370b6f81daf6465c12825778468fd5bf8c815
Roman <roman.gorelik@gmail.com>
2022-10-28 19:29:17 +0300

    WIP run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,5 @@ gcc decode.cpp -lavdevice -lavfilter -lavformat -lavcodec -lavutil -lpostproc -l
     -lswscale -lpthread -lz -llzma -lm -fpermissive -Llib -L/usr/local/lib/ -D__STDC_CONSTANT_MACROS -o my/decode \
     && echo "decode compiled" && ./my/decode ../video/P1033651.mp4 out/out || echo "Failed to compile decode"
 
-#cc decode.c -Llib -lavcodec -lavdevice -lavfilter -lavformat -lavutil -lpostproc -lswresample \
-#    -lswscale -lpthread -lz -llzma -lm -o my/decode \
-#    && echo "decode compiled" && ./my/decode ../video/P1033651.mp4 out/out || echo "Failed to compile decode"
-
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade Pillow
