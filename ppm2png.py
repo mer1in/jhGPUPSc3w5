1467c9ebf10177105c22ff184a0f68974d020761
Roman <roman.gorelik@gmail.com>
2022-10-28 19:51:14 +0300

    WIP ppm2png.py

--- ppm2png.py
+++ ppm2png.py
@@ -3,7 +3,7 @@ from PIL import Image
 from pathlib import Path
 
 
-inputFile = Path(sys.argv[0])
+inputFile = Path(sys.argv[1])
 print("Converting inputFile "+str(inputFile))
 Image.open(inputFile).save(inputFile.stem+'.png')
 
