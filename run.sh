e7d3267de7a08c3d4b7eda3700ac4b5e66a3173b
Roman <roman.gorelik@gmail.com>
2022-10-28 19:58:54 +0300

    WIP run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,12 @@ gcc decode.cpp -lavdevice -lavfilter -lavformat -lavcodec -lavutil -lpostproc -l
     -lswscale -lpthread -lz -llzma -lm -fpermissive -Llib -L/usr/local/lib/ -D__STDC_CONSTANT_MACROS -o my/decode \
     && echo "decode compiled" && ./my/decode ../video/P1033651.mp4 out/out || echo "Failed to compile decode"
 
+mkdir -p ~/project/boxFilterNPP/out
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade Pillow
-cd ..
-python3 ppm2png.py ./ffmpeg/out/fram.ppm
+
+for f in `ls out|grep ppm`
+do 
+    python3 ../ppm2png.py out/$f
+done
+cp out/*.png ~/project/boxFilterNPP/out/
