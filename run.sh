d41cb78868acdeb0558be7acc9e0ca84e15b2a7c
Roman <roman.gorelik@gmail.com>
2022-10-28 12:24:03 +0300

    test

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-cp decode.cpp ffmpeg/
+cp decode.c ffmpeg/
 cd ffmpeg
 mkdir out
+mkdir my
 #gcc decode.cpp -Llib -lavcodec -lavdevice -lavfilter -lavformat -lavutil -lpostproc -lswresample \
 #    -lswscale -lpthread -lz -llzma -lm -fpermissive -D__STDC_CONSTANT_MACROS \
 #    && echo "decode compiled" && ./a.out ../video/P1033651.mp4 out/out || echo "Failed to compile decode"
 
-cc decode.cpp -Llib -lavcodec -lavdevice -lavfilter -lavformat -lavutil -lpostproc -lswresample \
-    -lswscale -lpthread -lz -llzma -lm -fpermissive \
-    && echo "decode compiled" && ./a.out ../video/P1033651.mp4 out/out || echo "Failed to compile decode"
-    
+cc decode.c -Llib -lavcodec -lavdevice -lavfilter -lavformat -lavutil -lpostproc -lswresample \
+    -lswscale -lpthread -lz -llzma -lm -fpermissive -o my/decode \
+    && echo "decode compiled" && ./my/decode ../video/P1033651.mp4 out/out || echo "Failed to compile decode"
+
