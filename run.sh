9f0fc09104a962f538e21ad8e0d22f4d17c43d38
Roman <roman.gorelik@gmail.com>
2022-10-28 11:18:34 +0300

    WIP run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ cd ffmpeg
 mkdir out
 gcc decode.cpp -Llib -lavcodec -lavdevice -lavfilter -lavformat -lavutil -lpostproc -lswresample \
     -lswscale -lpthread -lz -llzma -lm -fpermissive \
-    && echo "decode compiled" || echo "Failed to compile decode" \
-    && ./a.out ../video/P1033651.mp4 out/out
+    && echo "decode compiled" && ./a.out ../video/P1033651.mp4 out/out || echo "Failed to compile decode"
+    
