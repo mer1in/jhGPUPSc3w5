da1baa479e45e21052f35a802db530101dd07899
Roman <roman.gorelik@gmail.com>
2022-10-28 10:09:42 +0300

    WIP run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 cp decode.cpp ffmpeg/
 cd ffmpeg
 gcc decode.cpp -Llib -lavcodec -lavdevice -lavfilter -lavformat -lavutil -lpostproc -lswresample \
-    -lswscale -lpthread -lz -llzma -lm
-
+    -lswscale -lpthread -lz -llzma -lm \
+    && echo "decode compiled" || echo "Failed to compile decode"
 ./a.out
