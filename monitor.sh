3d4a4c2ce2447e7fd6da359015ff4e5d84d4d0ab
Roman <roman.gorelik@gmail.com>
2022-10-28 10:04:32 +0300

    WIP monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -4,7 +4,7 @@ WHAT='./run.sh'
 while [[ true ]]
 do
     git pull -r >/dev/null
-    chsum2=`find . -type f ! -name "\.*\.sw*" -exec md5 {} \;`
+    chsum2=`find . -type f ! -name "\.*\.sw*" -exec md5sum {} \;`
     if [[ $chsum1 != $chsum2 ]] ; then
         chsum1=$chsum2
         (echo "$WHAT" | bash -)
