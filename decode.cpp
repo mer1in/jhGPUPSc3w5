aef556bc6b243ce29e9e98476663d22560a30027
Roman <roman.gorelik@gmail.com>
2022-11-03 15:19:46 +0200

    WIP decode.cpp

--- decode.cpp
+++ decode.cpp
@@ -74,28 +74,6 @@ static AVPacket *pkt = NULL;
 static int video_frame_count = 0;
 static int audio_frame_count = 0;
 
-#define FACE_DETECTION_CONFIGURATION "assets/deploy.prototxt"
-#define FACE_DETECTION_WEIGHTS "assets/res10_300x300_ssd_iter_140000_fp16.caffemodel"
-
-class FaceDetector { 
-public:     
-explicit FaceDetector();
-    std::vector<cv::Rect> detect_face_rectangles(const cv::Mat &frame);
-private:     
-/// Face detection network     
-cv::dnn::Net network_;     
-/// Input image width     
-const int input_image_width_;     
-/// Input image height     
-const int input_image_height_;     
-/// Scale factor when creating image blob     
-const double scale_factor_;     
-/// Mean normalization values network was trained with     
-const cv::Scalar mean_values_;     
-/// Face detection confidence threshold     
-const float confidence_threshold_;  
-};
-
 FaceDetector::FaceDetector() :
      confidence_threshold_(0.5),
       input_image_height_(300),
