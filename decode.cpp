fe87748782cf902d07badb30170e0654e5a5c16a
Roman <roman.gorelik@gmail.com>
2022-11-03 17:08:11 +0200

    WIP decode.cpp

--- decode.cpp
+++ decode.cpp
@@ -215,14 +215,15 @@ static int decode_packet(AVCodecContext *dec, const AVPacket *pkt)
                         pBGRFrame->data,        //uint8_t* const dst[], 
                         pBGRFrame->linesize);   //const int dstStride[]);
 
-        if (sts != frame->height)
-        {
-            printf("XXX sws_scale err\n");
-            return;  //Error!
-        } 
+            if (sts != frame->height)
+            {
+                printf("XXX sws_scale err\n");
+                return;  //Error!
+            } 
             cv::Mat img = cv::Mat(pBGRFrame->height, pBGRFrame->width,
                 CV_8UC3, pBGRFrame->data[0], pBGRFrame->linesize[0]);
             cv::imwrite(filename_buf, img);
+
         }
 
         sws_freeContext(sws_ctx);
