57f9ea03f4ca499a3ac1a7e18b8ee7a4431dfe51
Roman <roman.gorelik@gmail.com>
2022-11-03 17:41:53 +0200

    WIP decode.cpp

--- decode.cpp
+++ decode.cpp
@@ -83,7 +83,7 @@ FaceDetector::FaceDetector() :
      mean_values_({104., 177.0, 123.0}) {
          // Note: The variables MODEL_CONFIGURATION_FILE
          // and MODEL_WEIGHTS_FILE are passed in via cmake
-         network_ = cv::dnn::readNetFromCaffe(FACE_DETECTION_CONFIGURATION);
+         network_ = cv::dnn::readNetFromCaffe(FACE_DETECTION_CONFIGURATION, FACE_DETECTION_WEIGHTS);
       if (network_.empty()) {
          std::ostringstream ss;
          ss << "Failed to load network with the following settings:\n"
