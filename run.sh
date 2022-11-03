f5d89096668d725a763eec04465d927764083cc0
Roman <roman.gorelik@gmail.com>
2022-11-03 16:45:31 +0200

    WIP run.sh

--- run.sh
+++ run.sh
@@ -9,13 +9,15 @@ cd ffmpeg
 [ -d out ] || mkdir out
 [ -d my ] || mkdir my
 gcc decode.cpp -lavdevice -lavfilter -lavformat -lavcodec -lavutil -lpostproc -lswresample \
-    -lswscale -lpthread \
-    -lopencv_core -lopencv_imgcodecs -lopencv_dnn\
-    -lz -llzma -lm -fpermissive -lstdc++ -Llib \
-    -L${BASE_DIR}/libraries/lib \
-    -L/usr/local/lib/ -D__STDC_CONSTANT_MACROS -o my/decode \
-    -I${BASE_DIR}/libraries/include/opencv4 \
-    && echo "decode compiled" && LD_LIBRARY_PATH=${BASE_DIR}/libraries/lib ./my/decode ../video/P1033651.mp4 out/out || echo "Failed to compile decode"
+        -lswscale -lpthread \
+        -lopencv_core -lopencv_imgcodecs -lopencv_dnn\
+        -lz -llzma -lm -fpermissive -lstdc++ -Llib \
+        -L${BASE_DIR}/libraries/lib \
+        -L/usr/local/lib/ -D__STDC_CONSTANT_MACROS -o my/decode \
+        -I${BASE_DIR}/libraries/include/opencv4 \
+    && echo "decode compiled" \
+    && LD_LIBRARY_PATH=${BASE_DIR}/libraries/lib ./my/decode ../video/P1033651.mp4 out/out \
+    || echo "Failed to compile decode"
 
 #mkdir -p ~/project/boxFilterNPP/out
 #python3 -m pip install --upgrade pip
