6d0e09c4b8f3b4323acac685d14045c17eeebe58
Roman <roman.gorelik@gmail.com>
2022-11-04 08:34:58 +0200

    WIP run.sh

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ cd ffmpeg
 [ -d my ] || mkdir my
 gcc decode.cpp -lavdevice -lavfilter -lavformat -lavcodec -lavutil -lpostproc -lswresample \
         -lswscale -lpthread \
-        -lopencv_core -lopencv_imgcodecs -lopencv_dnn\
+        -lopencv_core -lopencv_imgcodecs -lopencv_dnn -lopencv_imgproc \
         -lz -llzma -lm -fpermissive -lstdc++ -Llib \
         -L${BASE_DIR}/libraries/lib \
         -L/usr/local/lib/ -D__STDC_CONSTANT_MACROS -o my/decode \
