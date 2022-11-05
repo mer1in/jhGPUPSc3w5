2b41f0fe278c099f47cdc56f8ab2c3c8e555793d
Roman <roman.gorelik@gmail.com>
2022-11-05 21:34:56 +0200

    speed up the monitor

--- monitor.sh
+++ monitor.sh
@@ -7,15 +7,14 @@ do
     echo -n .
     git pull -r &>/dev/null
     echo -n .
-    chsum2=`find . -type d \( -path ./ffmpeg/out -o -path ./ffmpeg/my \) -prune -o \
-        -type f ! -name "\.*\.sw*" -exec md5sum {} \;`
-#    chsum2=`find . -type f ! -name "\.*\.sw*" -exec md5sum {} \;`
+    chsum2=`git log -1 --oneline`
     if [[ $chsum1 != $chsum2 ]]
     then
         echo "Got changes, rebuild"
         chsum1=$chsum2
         echo $WHAT
         (echo "$WHAT" | bash -)
+        echo $chsum1
     fi
     echo -n .
     sleep 2
