90c9beffd93a46f44954ff0336ccecc3d04c8842
Roman <roman.gorelik@gmail.com>
2022-11-05 22:31:48 +0200

    WIP decode.hpp

--- decode.hpp
+++ decode.hpp
@@ -1,10 +1,8 @@
 #include <sstream> 
 #include <vector> 
 #include <string> 
-//#include <FaceDetector.h> 
 #include <opencv2/opencv.hpp>  
 
 #include "face_detector.hpp"
 
 int handle_video(const char *src_filename, const char *video_dst_filename);
-
