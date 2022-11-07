4b4e55d47fc315276ab2390719d305b79c3b5102
Roman <roman.gorelik@gmail.com>
2022-11-07 15:45:51 +0200

    WIP ./decode.cpp

--- decode.cpp
+++ decode.cpp
@@ -180,7 +180,7 @@ static int decode_packet(AVCodecContext *dec, const AVPacket *pkt)
             for(const auto & r : rectangles){
 
                 NppiSize isize = {r.width, r.height};
-                Npp8u *face_addr = dev_mem + (r.y * r.width * 3 + r..x * 3);
+                Npp8u *face_addr = dev_mem + (r.y * r.width * 3 + r.x * 3);
                 printf("face_addr x = %d y = %d\n", r.x, r.y);
                 nppiFilterGauss_8u_C3R(face_addr, pBGRFrame->width*3, face_addr, pBGRFrame->width*3, isize, NPP_MASK_SIZE_9_X_9);
 
