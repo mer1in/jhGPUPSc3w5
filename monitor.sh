645e8427899351ed72a7f3c1cffec5938b9aa141
Roman <roman.gorelik@gmail.com>
2022-11-07 15:09:48 +0200

    WIP monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -4,9 +4,7 @@ WHAT='./run.sh'
 
 while [[ true ]]
 do
-    echo -n .
     git pull -r &>/dev/null
-    echo -n .
     chsum2=`git log -1 --oneline`
     if [[ $chsum1 != $chsum2 ]]
     then
@@ -16,6 +14,6 @@ do
         (echo "$WHAT" | bash -)
         echo $chsum1
     fi
-    echo -n .
     sleep 2
+    echo -n .
 done
