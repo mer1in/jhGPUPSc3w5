17806b4117b277550ca2ea3231bf45089b707b82
Roman <roman.gorelik@gmail.com>
2022-11-12 19:18:35 +0200

    WIP init.sh

--- init.sh
+++ init.sh
@@ -28,14 +28,6 @@ cd ffmpeg && \
     make install
 )
 
-echo "Run decoding test"
-(
-mkdir -p ~/project/out
-./ffmpeg/bin/ffmpeg -i ./video/P1033651.mp4 mpeg1.mpeg
-./ffmpeg/doc/examples/decode_video ./mpeg1.mpeg ~/project/out/frame
-ls -la ~/project/out/
-)
-
 echo "--- Build openCV ---"
 (
 cd opencv
