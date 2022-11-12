ebdb6651eff374000bca404a8e6ed93ed45a37b9
Roman <roman.gorelik@gmail.com>
2022-11-12 16:13:15 +0200

    WIP /Users/romangorelik/study/coursera/cuda-at-scale-for-the-enterprise/week5-fin-proj/init.sh

--- init.sh
+++ init.sh
@@ -5,16 +5,24 @@ BASE_DIR=$(pwd)
 echo "Fetch submodules"
 git submodule update --init --recursive
 
+echo "Build libx264"
+(
+cd x264
+PATH="$BASE_DIR/libraries/bin:$PATH" \
+    ./configure --prefix="$BASE_DIR/libraries" \
+        --bindir="$BASE_DIR/libraries/bin" --enable-static --enable-pic \
+        --disable-asm && \
+    PATH="$BASE_DIR/libraries/bin:$PATH" make && \
+    make install
+)
 
-# PATH="$HOME/bin:$PATH" ./configure --prefix="`pwd`/../ffmpeg/" --bindir="`pwd`/../ffmpeg/bin" --enable-static --enable-pic --disable-asm && PATH="$HOME/bin:$PATH" make && make install
 echo "Build ffmpeg"
 (
-
-#  cd ffmpeg && ./configure --enable-static --enable-pic --enable-nonfree \ --enable-shared --enable-libx264 --enable-libx265 
-
-cd ffmpeg && ./configure --enable-static --enable-pic --enable-nonfree \
-    --enable-gpl --disable-x86asm --prefix=$BASE_DIR/libraries \
-    && make && \
+cd ffmpeg && \
+    ./configure --enable-static --enable-shared --enable-pic --enable-nonfree \
+        --enable-libx264 --enable-gpl  --disable-x86asm \
+        --prefix=$BASE_DIR/libraries && \
+    make && \
     make install
 )
 
