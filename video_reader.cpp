41f4cc4ad1bc7c67080e6d1e7e06767a20850cc9
Roman <roman.gorelik@gmail.com>
2022-11-16 16:30:14 +0200

    WIP video_reader.cpp

--- video_reader.cpp
+++ video_reader.cpp
@@ -1,7 +1,14 @@
 #include "video_reader.hpp"
 
 VideoReader::VideoReader(std::string file_name) {
+
     if (avformat_open_input(&fmt_ctx, file_name.c_str(), NULL, NULL) < 0)
         throw(Exception("Could not open source file " + file_name));
+
+    if (avformat_find_stream_info(fmt_ctx, NULL) < 0)
+        throw(Exception("Could not find stream information"));
     
+    if (!( codec = avcodec_find_encoder_by_name("libx264") ))
+        throw(Exception("Codec libx264 not found"));
+
 }
