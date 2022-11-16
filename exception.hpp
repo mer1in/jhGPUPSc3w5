5d2fbaf6959b44d008d029311af5352f9128b6f5
Roman <roman.gorelik@gmail.com>
2022-11-16 15:42:36 +0200

    WIP exception.hpp

---
+++ exception.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Exception {
+
+public:
+   Exception(const string& msg) : msg_(msg) {}
+  ~Exception() {}
+
+   string getMessage() const {return(msg_);}
+private:
+   string msg_;
+};
+
