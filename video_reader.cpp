0f8149fba98abf53c3d289731655a856b1d5ad85
Roman <roman.gorelik@gmail.com>
2022-11-16 16:25:25 +0200

    WIP video_reader.cpp

--- video_reader.cpp
+++ video_reader.cpp
@@ -1,7 +1,7 @@
 #include "video_reader.hpp"
 
 VideoReader::VideoReader(std::string file_name) {
-    if (avformat_open_input(&fmt_ctx, file_name, NULL, NULL) < 0)
+    if (avformat_open_input(&fmt_ctx, file_name.c_str(), NULL, NULL) < 0)
         throw(Exception("Could not open source file " + file_name));
     
 }
