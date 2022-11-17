4193936b158f6d577fc49cf923f44108c545f91e
Roman <roman.gorelik@gmail.com>
2022-11-17 10:12:25 +0200

    WIP video_reader.cpp

--- video_reader.cpp
+++ video_reader.cpp
@@ -6,13 +6,7 @@ VideoReader::VideoReader(std::string file_name) {
     const AVCodec *dec = NULL;
     AVStream *st;
 
-    try{
     throw(Exception("XXX"));
-    }
-    catch(Exception& e){
-        printf("AAAAAA\n");
-        cout<<"XXX";
-    }
     
     if (avformat_open_input(&fmt_ctx, file_name.c_str(), NULL, NULL) < 0)
         throw(Exception("Could not open source file " + file_name));
