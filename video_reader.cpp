2540fc9c0e2bc8b1156a4bb64acf21f7f61d1e8b
Roman <roman.gorelik@gmail.com>
2022-11-17 09:24:21 +0200

    WIP video_reader.cpp

--- video_reader.cpp
+++ video_reader.cpp
@@ -3,6 +3,8 @@
 VideoReader::VideoReader(std::string file_name) {
 
     int stream_index;
+    const AVCodec *dec = NULL;
+    AVStream *st;
 
     if (avformat_open_input(&fmt_ctx, file_name.c_str(), NULL, NULL) < 0)
         throw(Exception("Could not open source file " + file_name));
@@ -10,7 +12,7 @@ VideoReader::VideoReader(std::string file_name) {
     if (avformat_find_stream_info(fmt_ctx, NULL) < 0)
         throw(Exception("Could not find stream information"));
     
-    if (!( codec = avcodec_find_encoder_by_name("libx264") ))
+    if (!(codec = avcodec_find_encoder_by_name("libx264")))
         throw(Exception("Codec libx264 not found"));
 
 //    if (open_codec_context(&video_stream_idx, &video_dec_ctx, fmt_ctx, AVMEDIA_TYPE_VIDEO) >= 0)
@@ -20,6 +22,6 @@ VideoReader::VideoReader(std::string file_name) {
     if((stream_index = av_find_best_stream(fmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0)) < 0)
         throw(Exception("Could not find "+string(av_get_media_type_string(AVMEDIA_TYPE_VIDEO))+" stream in input file"));
     
-
+    st = fmt_ctx->streams[stream_index];
 
 }
