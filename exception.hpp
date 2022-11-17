689750a8d50390ca7a5fe47850f8945ebe072323
Roman <roman.gorelik@gmail.com>
2022-11-17 10:10:18 +0200

    WIP exception.hpp

--- exception.hpp
+++ exception.hpp
@@ -4,13 +4,13 @@
 
 using namespace std;
 
-class Exception {
+class Exception : public exception {
 
 public:
    Exception(const string& msg) : msg_(msg) {}
   ~Exception() {}
 
-   string getMessage() const {return(msg_);}
+   const char* what() {return(msg_.c_str());}
 private:
    string msg_;
 };
