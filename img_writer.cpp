8abed976f8019dee1c90c38316054e8d561e10f9
Roman <roman.gorelik@gmail.com>
2022-11-19 16:52:27 +0200

    WIP img_writer.cpp

--- img_writer.cpp
+++ img_writer.cpp
@@ -5,11 +5,11 @@ ImgWriter::ImgWriter(string input_file_name, string suffix)
     size_t pos = input_file_name.find('.');
     if (pos == string::npos)
         pos = input_file_name.length() - 1;
-    file_name = input_file_name.substr(0, pos) + "-" +suffix;
+    file_name = input_file_name.substr(0, pos) + "-" +suffix + "-";
 }
 
 int ImgWriter::save(cv::Mat img)
 {
-    string fn = file_name;
-
+    string fn = file_name + to_string(num) + ".jpg";
+    cv::imwrite(fn.c_str(), img);
 }
