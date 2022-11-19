63599973349b76916b56c0cf06a2cb2982a8ca0a
Roman <roman.gorelik@gmail.com>
2022-11-19 11:35:26 +0200

    WIP img_writer.cpp

--- img_writer.cpp
+++ img_writer.cpp
@@ -3,7 +3,7 @@
 ImgWriter::ImgWriter(string input_file_name, string suffix)
 {
     size_t pos = input_file_name.find('.');
-    if (pos == npos)
+    if (pos == string::npos)
         pos = input_file_name.length() - 1;
     file_name = input_file_name.substr(0, pos) + "-" +suffix;
 }
