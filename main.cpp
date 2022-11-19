58b66c12952d4d0025a0b149ff7460230529f33a
Roman <roman.gorelik@gmail.com>
2022-11-19 07:50:23 +0200

    WIP main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@
 
 void usage(char* name)
 {
-    fprintf(stderr, "usage: %s <input_file> <output_file> [--verbose] [--dump-input] [--dump-blured]\n"
+    fprintf(stderr, "usage: %s <input_file> <output_file> [--dump-input] [--dump-blured]\n"
         "This program searchs for faces in input video and blurs them\n"
         "  input_file     video file to be processed, required\n"
         "  output_file    name of a file to write results to, required\n"
-        "  --verbose      verbose output"
         "  --dump-input   write each input frame to a file named <output_file>-input-<frame-number>.jpg"
         "  --dump-blured  write each blured frame to a file named <output_file>-blured-<frame-number>.jpg",
         name);
@@ -19,8 +18,15 @@ int main (int argc, char **argv)
 {
     if (argc < 3)
         usage(argv[0]);
-
+    
     auto processor = new VideoProcessor(argv[1], argv[2]);
+    for (int i=3; i<argc; i++)
+    {
+        if (string(argv[i]).compare("--dump-input"))
+            processor->setDumpInput(true);
+        if (string(argv[i]).compare("--dump-blured"))
+            processor->setDumpBlured(true);
+    }
     processor->run();
     delete(processor);
 //    return handle_video(argv[1], argv[2]);
