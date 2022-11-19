ac1515840bca3b0e88356d93daa08b4a9f5cf1a2
Roman <roman.gorelik@gmail.com>
2022-11-19 11:24:19 +0200

    WIP img_writer.hpp

--- img_writer.hpp
+++ img_writer.hpp
@@ -6,7 +6,7 @@ class ImgWriter {
         string file_name;
         int num = 1;
     public:
-        ImgWriter(string file_name);
-        ~ImgWriter();
+        ImgWriter(string input_file_name);
+        ~ImgWriter(){}
         int save();
 }
