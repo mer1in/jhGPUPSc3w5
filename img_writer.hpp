e43e1eb8815af086c1853238ecbc7942e1452919
Roman <roman.gorelik@gmail.com>
2022-11-19 11:04:57 +0200

    WIP img_writer.hpp

--- img_writer.hpp
+++ img_writer.hpp
@@ -1,2 +1,12 @@
+#pragma once
 #include <opencv2/opencv.hpp> 
 
+class ImgWriter {
+    private:
+        string file_name;
+        int num = 1;
+    public:
+        ImgWriter(string file_name);
+        ~ImgWriter();
+        int save();
+}
