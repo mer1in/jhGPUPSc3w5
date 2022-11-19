d9a2d3b8106295539cd6eb887c4db4022231b6ae
Roman <roman.gorelik@gmail.com>
2022-11-19 11:24:12 +0200

    WIP img_writer.cpp

--- img_writer.cpp
+++ img_writer.cpp
@@ -1,3 +1,17 @@
 #include "img_writer.hpp"
 
+use namespace std;
 
+ImgWriter::ImgWriter(input_file_name, suffix)
+{
+    size_t pos = input_file_name.find('.');
+    if (pos == npos)
+        pos = input_file_name.length() - 1;
+    file_name = input_file_name.substr(0, pos) + "-" +suffix;
+}
+
+save(cv::Mat img)
+{
+    string fn = file_name;
+
+}
