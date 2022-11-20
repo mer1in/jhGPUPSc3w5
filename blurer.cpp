7fc716b93eaa940e2e38778952812d1296133d19
Roman <roman.gorelik@gmail.com>
2022-11-20 09:08:15 +0200

    WIP blurer.cpp

--- blurer.cpp
+++ blurer.cpp
@@ -1,10 +1,9 @@
 #include "blurer.hpp"
 void Blurer::blur(std::vector<cv::Rect> faces, cv::Mat img)
 {
-
+    size_t size = 3 * img.size().width * img.size().height;
     if (!dev_mem)
     {
-        size_t size = 3 * img.size().width * img.size().height;
         cudaError_t err = cudaMalloc(&dev_mem, size);
         if (err != cudaSuccess)
         {
