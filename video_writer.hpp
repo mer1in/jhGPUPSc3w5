16f6891cb4ff77db53a0b6b4cb09c3f1838818d7
Roman <roman.gorelik@gmail.com>
2022-11-20 12:08:09 +0200

    WIP video_writer.hpp

--- video_writer.hpp
+++ video_writer.hpp
@@ -1,6 +1,11 @@
 #include <string>
 #include <opencv2/opencv.hpp> 
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+}
 using namespace std;
 class VideoWriter{
     public:
