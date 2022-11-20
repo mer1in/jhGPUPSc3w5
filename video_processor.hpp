128527dbf374c33c2c8e3f779911d705d9d41f17
Roman <roman.gorelik@gmail.com>
2022-11-20 05:46:31 +0200

    WIP video_processor.hpp

--- video_processor.hpp
+++ video_processor.hpp
@@ -17,7 +17,7 @@ class VideoProcessor {
         ImgWriter input_img_writer;
         ImgWriter blured_img_writer;
     public:
-        VideoProcessor(string src_filename, string dst_filename, strig dump_input, string dump_blured);
+        VideoProcessor(string src_filename, string dst_filename, string dump_input, string dump_blured);
         ~VideoProcessor();
         int run();
 };
