7a081449e00a1b50e8a194816aa3605ad0afddd3
Roman <roman.gorelik@gmail.com>
2022-11-20 12:08:57 +0200

    WIP video_writer.cpp

--- video_writer.cpp
+++ video_writer.cpp
@@ -9,9 +9,7 @@ void VideoWriter::write(cv::Mat img){
 VideoWriter::VideoWriter(string file_name) : file_name(file_name)
 {
     pkt_enc = av_packet_alloc();
-    if (!pkt || !pkt_enc) {
+    if (!pkt_enc) {
         fprintf(stderr, "Could not allocate packet\n");
-        ret = AVERROR(ENOMEM);
-        goto end;
     }
 };
