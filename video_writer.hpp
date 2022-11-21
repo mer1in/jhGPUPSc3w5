917c9d168a65b6fde09f1ae6c9002563979d78da
Roman <roman.gorelik@gmail.com>
2022-11-21 06:19:44 +0200

    WIP video_writer.hpp

--- video_writer.hpp
+++ video_writer.hpp
@@ -2,6 +2,7 @@
 #include <opencv2/opencv.hpp> 
 #include "exception.hpp"
 extern "C" {
+#include <libavutil/opt.h>
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
