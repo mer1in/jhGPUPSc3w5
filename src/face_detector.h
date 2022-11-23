6b657d29f50395ff99d6433e2d093eff0f3826d7
Roman <roman.gorelik@gmail.com>
2022-11-23 18:26:42 +0200

    WIP face_detector.h

--- src/face_detector.h
+++ src/face_detector.h
@@ -9,12 +9,12 @@ class FaceDetector {
         explicit FaceDetector();
         std::vector<cv::Rect> detect(const cv::Mat &frame);
     private:     
-        cv::dnn::Net network_;     
-        const int input_image_width_;     
-        const int input_image_height_;     
-        const double scale_factor_;     
-        const cv::Scalar mean_values_;     
-        const float confidence_threshold_;  
+        cv::dnn::Net network;     
+        const int inputImageWidth;     
+        const int inputImageHeight;     
+        const double scaleFactor;     
+        const cv::Scalar meanValues;     
+        const float confidenceThreshold;  
 };
 
 #define FACE_DETECTION_CONFIGURATION "assets/deploy.prototxt"
