f48f2222c3f6b6ab7ad7f5e5fc07d4bde7e636a2
Roman <roman.gorelik@gmail.com>
2022-11-23 18:26:30 +0200

    WIP face_detector.cpp

--- src/face_detector.cpp
+++ src/face_detector.cpp
@@ -19,35 +19,35 @@ FaceDetector::FaceDetector() :
 
 std::vector<cv::Rect> FaceDetector::detect(const cv::Mat &frame)
 {
-    cv::Mat input_blob = cv::dnn::blobFromImage(frame, scale_factor_,
-        cv::Size(input_image_width_, input_image_height_), mean_values_,
+    cv::Mat inputBlob = cv::dnn::blobFromImage(frame, scaleFactor,
+        cv::Size(inputImageWidth, inputImageHeight), meanValues,
         false, false);
-    network_.setInput(input_blob, "data");
-    cv::Mat detection = network_.forward("detection_out");
-    cv::Mat detection_matrix(detection.size[2], detection.size[3], CV_32F,
+    network.setInput(inputBlob, "data");
+    cv::Mat detection = network.forward("detection_out");
+    cv::Mat detectionMatrix(detection.size[2], detection.size[3], CV_32F,
         detection.ptr<float>());
     std::vector<cv::Rect> faces;
 
-    for (int i = 0; i < detection_matrix.rows; i++)
+    for (int i = 0; i < detectionMatrix.rows; i++)
     {
-        float confidence = detection_matrix.at<float>(i, 2);
+        float confidence = detectionMatrix.at<float>(i, 2);
         if (confidence < confidenceThreshold)
             continue;
         
-        int x_left_bottom = static_cast<int>(
-            detection_matrix.at<float>(i, 3) * frame.cols);
+        int xLeftBottom = static_cast<int>(
+            detectionMatrix.at<float>(i, 3) * frame.cols);
 
-        int y_left_bottom = static_cast<int>(
-            detection_matrix.at<float>(i, 4) * frame.rows);
+        int yLeftBottom = static_cast<int>(
+            detectionMatrix.at<float>(i, 4) * frame.rows);
 
-        int x_right_top = static_cast<int>(
-            detection_matrix.at<float>(i, 5) * frame.cols);
+        int xRightTop = static_cast<int>(
+            detectionMatrix.at<float>(i, 5) * frame.cols);
 
-        int y_right_top = static_cast<int>(
-            detection_matrix.at<float>(i, 6) * frame.rows);
+        int yRightTop = static_cast<int>(
+            detectionMatrix.at<float>(i, 6) * frame.rows);
 
-        faces.emplace_back(x_left_bottom, y_left_bottom,
-            (x_right_top - x_left_bottom), (y_right_top - y_left_bottom));
+        faces.emplace_back(xLeftBottom, yLeftBottom,
+            (xRightTop - xLeftBottom), (yRightTop - yLeftBottom));
     }
 
     return faces;
