b7471769bd6e463b926bedd2c1388bfa5a3e5a2f
Roman <roman.gorelik@gmail.com>
2022-11-23 06:16:12 +0200

    change folders stru

--- ppm2png.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PIL import Image
-from pathlib import Path
-
-
-inputFile = Path(sys.argv[1])
-print("Converting inputFile "+str(inputFile))
-Image.open(inputFile).save((inputFile.parent /  inputFile.stem)+'.png')
-
